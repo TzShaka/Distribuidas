bb1ec3948977a84ee90fcb5c14df151e1ec3648c
TzShaka <brayanhyo@gmail.com>
2025-05-12 12:41:44 -0500

    Auth

---
+++ ms_cliente_service/src/main/java/com/example/ms_cliente/MsClienteServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.ms_cliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class x          MsClienteServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsClienteServiceApplication.class, args);
+    }
+
+}
