1eebcf480e283eaaddd82080a9c41004347b86a1
TzShaka <brayanhyo@gmail.com>
2025-05-12 15:49:27 -0500

    Cambios Auth

--- ms_catalogo/src/main/java/com/example/ms_catalogo/MsCatalogoApplication.java
+++ ms_catalogo/src/main/java/com/example/ms_catalogo/MsCatalogoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class xMsCatalogoApplication {
+public class MsCatalogoApplication {
     public static void main(String[] args) {
         SpringApplication.run(MsCatalogoApplication.class, args);
     }
--- ms_cliente_service/src/main/java/com/example/ms_cliente/MsClienteServiceApplication.java
+++ ms_cliente_service/src/main/java/com/example/ms_cliente/MsClienteServiceApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class x          MsClienteServiceApplication {
+public class MsClienteServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MsClienteServiceApplication.class, args);
